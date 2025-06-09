30ca9f4036e6ef018674d8cab3a877356048f37c
yzr-swag <yozertv@gmail.com>
2025-06-09 18:50:51 +0100

    Player and entity classes

--- src/main/java/yzr/tilemapping/GamePanel.java
+++ src/main/java/yzr/tilemapping/GamePanel.java
@@ -2,28 +2,42 @@ package yzr.tilemapping;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
+import yzr.entity.Player;
+
 public class GamePanel extends JPanel implements Runnable{
     
-    //Screen settings - cpuld just change to 64x?
-    //final int originalTileSize = 16;
-    //final int scale = 3;
+    final int originalTileSize = 16;
+    final int scale = 4;
 
-    final int tileSize = 64;
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol;
-    final int screenHeight = tileSize * maxScreenRow;
+    public final int tileSize = originalTileSize * scale;
+    final int maxScreenCol = 12;
+    final int maxScreenRow = 24;
+    final int screenWidth = tileSize * maxScreenRow;
+    final int screenHeight = tileSize * maxScreenCol;
 
+    int FPS= 60;
+
+    public KeyHandler keyH = new KeyHandler();
     Thread gameThread;
+    Player player = new Player(this, keyH);
+
+    //set default position
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
 
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-        this.setBackground(Color.BLACK);
+        this.setBackground(Color.black);
         this.setDoubleBuffered(true);
+        this.addKeyListener(keyH);
+        this.setFocusable(true);
     }
 
     public void startGameThread() {
@@ -34,8 +48,59 @@ public class GamePanel extends JPanel implements Runnable{
 
 
     @Override
+    @SuppressWarnings("CallToPrintStackTrace")
     public void run() {
         
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        long timer = 0;
+        int drawCount = 0;
+
+        while(gameThread != null) {
+
+            currentTime = System.nanoTime();
+            
+            delta += (currentTime - lastTime)  / drawInterval;
+            timer += (currentTime - lastTime);
+            lastTime = currentTime;
+
+            if (delta >= 1) {
+                update();
+                repaint();
+                delta --;
+                drawCount ++;
+            }
+
+            if (timer >= 1000000000) {
+                System.out.println("FPS:" + drawCount);
+                drawCount = 0;
+                timer = 0;
+            } 
+
+        }
+
+    }
+
+
+    public void update() {
+
+        player.update();
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D)g;
+
+        player.draw(g2);
+
+        //g2.dispose();
+
     }
 
     }
--- src/main/java/yzr/tilemapping/TilemappingGame.java
+++ src/main/java/yzr/tilemapping/TilemappingGame.java
@@ -16,32 +16,22 @@ public class TilemappingGame {
 
     public static void main(String[] args) {
         JFrame window = new JFrame();
+        GamePanel gamePanel = new GamePanel();
+
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
         window.setTitle("TileMapping Game");
 
-        GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
 
         window.pack();
 
         window.setLocationRelativeTo(null);
         window.setVisible(true);
+
+        gamePanel.startGameThread();
     }
 
 }
 
-
-
-    //public static void main(String[] args) {
-    //    Tile[][] map = new Tile[50][50];
-    //    
-
-    //    for (int i = 0; i < 50; i++) {
-    //        for (int j = 0; j < 50; j++) {
-    //            map[i][j] = new Tile();
-    //        }
-    //    }
-        
-    //    map[1][1].fileTest();
 
